574032bb4b461d1bd257f88e94eee476de895c17
Ridzki R Alfaza <80326349+Alffaza@users.noreply.github.com>
2022-12-10 17:47:40 +0700

    no 6

--- script/garden.sh
+++ script/garden.sh
@@ -4,6 +4,14 @@ nameserver 192.168.122.1
 
 apt-get install netcat -y
 
+iptables -F
+
 # soal 4
 iptables -A INPUT -d 10.10.6.139/29 -m time --timestart 07:00 --timestop 16:00 --weekdays Mon,Tue,Wed,Thu,Fri -j ACCEPT
 iptables -A INPUT -d 10.10.6.139/29 -j REJECT
+
+iptables -N LOGGING
+iptables -A INPUT -j LOGGING
+iptables -A OUTPUT -j LOGGING
+iptables -A LOGGING -j LOG --log-prefix "IPTables-Dropped: " --log-level 4
+iptables -A LOGGING -j DROP
